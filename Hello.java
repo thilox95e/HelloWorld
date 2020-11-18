1d537754b17598173cca760adaabca0541db3655
Ubuntu <ubuntu@ip-172-31-29-98.ap-southeast-1.compute.internal>
2020-11-18 09:58:43 +0000

    added hello world java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+	public static void main(String[] args) throws Exception {
+
+		for(int i = 1; i<= 10; i++) {
+			System.out.println("Hello World : "+i);
+			Thread.sleep(1000);
+		}
+
+	}
+
+}
